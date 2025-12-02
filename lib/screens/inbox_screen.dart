8ae4485cd2e2c15a5d62fa4d8008f2149f233b4b
Babatundé Adissa Fadolle Arouna <300151970@monboreal.ca>
2025-12-02 17:04:36 -0500

    initialisation du code du inbox_screen

---
+++ lib/screens/inbox_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../db/email_database.dart';
+import 'compose_screen.dart';
+import 'settings_screen.dart';
+
+class InboxScreen extends StatefulWidget {
+  @override
+  _InboxScreenState createState() => _InboxScreenState();
+}
