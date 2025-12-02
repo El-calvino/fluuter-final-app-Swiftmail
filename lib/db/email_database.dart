4c0ef012499048a48d7996dca46120b864f554d8
El-Calvino <kiegaingkouamcalvin@gmail.com>
2025-12-02 16:28:05 -0500

    Finalisation du code de gestion de la base

--- lib/db/email_database.dart
+++ lib/db/email_database.dart
@@ -18,3 +18,34 @@ class EmailDatabase {
     final path = join(dbPath, filePath);
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE emails (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        subject TEXT NOT NULL,
+        content TEXT NOT NULL,
+        date TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<int> insertEmail(String subject, String content) async {
+    final db = await instance.database;
+    return await db.insert('emails', {
+      'subject': subject,
+      'content': content,
+      'date': DateTime.now().toIso8601String()
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> getEmails() async {
+    final db = await instance.database;
+    return await db.query('emails', orderBy: 'date DESC');
+  }
+
+  Future<int> deleteEmail(int id) async {
+    final db = await instance.database;
+    return await db.delete('emails', where: 'id = ?', whereArgs: [id]);
+  }
+}
