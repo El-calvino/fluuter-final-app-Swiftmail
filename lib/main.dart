45c2691aca561aea3391e848cca36d14cda3f3af
Oussama-rgb-art <oussamazouggare123@gmail.com>
2025-12-03 16:31:40 -0500

    Finalisation du main et ajout de la fonction void main()

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,27 @@ import 'screens/login_screen.dart';
 import 'screens/inbox_screen.dart';
 import 'screens/compose_screen.dart';
 import 'screens/settings_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(SwiftMailApp());
+}
+
+class SwiftMailApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SwiftMail',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/inbox': (context) => InboxScreen(),
+        '/compose': (context) => ComposeScreen(onSend: () {}),
+        '/settings': (context) => SettingsScreen(),
+      },
+    );
+  }
+}
