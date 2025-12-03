8f0ae8c5e1445b53537e7e716e3ea901b97b3715
Oussama-rgb-art <oussamazouggare123@gmail.com>
2025-12-03 16:25:51 -0500

    Debut du code login Screen creation classe LoginScreenState

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isLoading = false;
