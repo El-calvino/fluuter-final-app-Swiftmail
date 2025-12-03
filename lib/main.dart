f04f8d30dce911737e49bb3db611f3c274709d0f
Oussama-rgb-art <oussamazouggare123@gmail.com>
2025-12-03 16:30:37 -0500

    Debut du main ajout des Import et paquets

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/login_screen.dart';
+import 'screens/inbox_screen.dart';
+import 'screens/compose_screen.dart';
+import 'screens/settings_screen.dart';
